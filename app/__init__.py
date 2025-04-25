cfbd4fbdfc2213ed174550469ced97a9d59695da
medericc <bleublanctour1@>
2025-04-25 08:51:10 +0200

    ee

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
-from flask_migrate import Migrate  # Import de Flask-Migrate
+from flask_migrate import Migrate 
 from config import Config
 
-# Initialisation de SQLAlchemy, JWTManager et Flask-Migrate
 db = SQLAlchemy()
 jwt = JWTManager()
-migrate = Migrate()  # Création de l'objet Migrate
+migrate = Migrate() 
 
 def create_app():
     app = Flask(__name__)
@@ -15,9 +14,12 @@ def create_app():
 
     db.init_app(app)
     jwt.init_app(app)
-    migrate.init_app(app, db)  # Initialisation de Flask-Migrate avec l'app et la base de données
+    migrate.init_app(app, db)
 
-    # Import des modèles
     from .models import user, category, topic, reply, like, follow, message, notification, forum, file
 
+    from .routes import register_routes
+    register_routes(app)
+
     return app
+
